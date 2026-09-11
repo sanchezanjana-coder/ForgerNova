42a632485b1babd9488ecf06597d2e4bff3199b1
sanchezanjana-coder <sanchezanjana@gmail.com>
2026-09-11 14:45:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -721,7 +721,7 @@ part.AssemblyLinearVelocity = part.CFrame.LookVector * SPEED`
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama-3.3-70b-versatile",
+       model: "llama3-70b-8192",
           messages: [
             {
               role: "system",
